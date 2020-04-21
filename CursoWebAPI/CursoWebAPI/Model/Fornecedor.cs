734272e1717ff8446d503b894c1f90b8b3c47500
unknown <luis.carlos.da.silva.marques@gmail.com>
2020-04-21 17:02:44 -0300

    Incluindo model e DbContext

---
+++ CursoWebAPI/CursoWebAPI/Model/Fornecedor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoWebAPI.Model
+{
+    public class Fornecedor
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {1} e {2} carateres", MinimumLength = 2)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(14, ErrorMessage = "O campo {0} precisa ter entre {1} e {2} caracteres", MinimumLength = 11)]
+        public string Documento { get; set; }
+
+        public int TipoFornecedor { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
